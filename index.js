150a03ba9d93d943023a64e6ad584a319598eb49
Keith Emery <kmne68@gmail.com>
2021-02-14 02:02:03 -0600

    Add API call functionality

--- index.js
+++ index.js
@@ -2,17 +2,44 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request')
 const PORT = process.env.PORT || 5000;
+
+
+// API KEY: pk_ae48454bd18a453599f01ffa35da75f7
+
+// Function to call the API
+function call_api(finishedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_ae48454bd18a453599f01ffa35da75f7', {json: true}, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        };
+        if (res.statusCode === 200) {
+            // console.log(body);
+            finishedAPI(body);
+        };
+    });
+};
 
 // Set Handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+
 // Handlebar routes
 app.get('/', function (req, res) {
-    res.render('home', {
-        example: "This is an example"
+    // use a callback to ensure time for data retrieval
+    call_api(function(completeAPI) {
+        res.render('home', {
+            stock: completeAPI
+        });
     });
+});
+
+
+// About page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
